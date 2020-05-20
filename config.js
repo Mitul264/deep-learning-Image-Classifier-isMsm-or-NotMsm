8b7a1f4f4862690b44666ed99ae9a067adf4d2ae
Mitul Patel <patelm5@myumanitoba.ca>
2020-05-20 13:53:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // The URI of the web service endpoint.  This should look soemthing like:
 // https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/prod
 
-var apiUrl = "https://0j5zqsv7q0.execute-api.us-east-1.amazonaws.com/prad"
+var apiUrl = "https://emk31wehee.execute-api.us-east-1.amazonaws.com/prod"
